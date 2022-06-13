fb5a42807624cd63ff00260920f1b11c0408efa5
victor amaral <victor.amaralb@hotmail.com>
2022-06-13 19:03:38 -0300

    game css

--- src/components/StartScreen/StartScreen.js
+++ src/components/StartScreen/StartScreen.js
@@ -1,10 +1,24 @@
 import React from "react";
 import "./StartScreen.css";
 
-const StartScreen = ({ startGame }) => {
+const StartScreen = ({ startGame, word }) => {
   return (
     <div className="start">
-      <h1>Secret word</h1>
+      <h1 className="secret_title">
+        Secret
+        <span
+          style={{
+            backgroundColor: "#0F0A0A",
+            marginLeft: 10,
+            color: "#FFF",
+            borderRadius: 15,
+            width: 180,
+          }}
+        >
+          {" "}
+          Word
+        </span>
+      </h1>
       <p>Clique no botão para começar a jogar</p>
       <button onClick={startGame}> Começar </button>
     </div>
